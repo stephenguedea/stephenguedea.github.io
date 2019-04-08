36164a37fc5ff97d039259421756b5e0d9cfa37d
Stephen Guedea <guedea.stephen@gmail.com>
2019-04-08 18:08:51 -0500

    updated weight-convert... added oz, grams, kg

--- js/weight-converter.js
+++ js/weight-converter.js
@@ -7,8 +7,8 @@ document.getElementById('lbsInput').addEventListener('input', function (e) {
     document.getElementById('output').style.visibility = 'visible';
 
     let lbs = e.target.value;
-    document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(2);
-    document.getElementById('kgOutput').innerHTML = (lbs/2.2046).toFixed(2);
+    document.getElementById('gramsOutput').innerHTML = (lbs * 453.592).toFixed(4);
+    document.getElementById('kgOutput').innerHTML = (lbs * 0.453592).toFixed(6);
     document.getElementById('ozOutput').innerHTML = lbs*16;
 
 
